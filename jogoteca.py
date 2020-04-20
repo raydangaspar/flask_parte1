3d687a22c73f39d2812a81fb99298d8a5886fb87
raydangaspar <raydangaspar@gmail.com>
2020-04-20 18:41:17 -0300

    utilizando render_template

--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/inicio')
 def ola():
-    return '<h1>Olá Flask</h1>'
+    # return '<h1>Olá Flask</h1>'   # não é uma boa ideia colocar html explicitamente no código
+    return render_template('lista.html')
 
 
 app.run()
