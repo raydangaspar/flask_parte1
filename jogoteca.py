20cae3edbd2bb49d01019875e92d2724e14d59b6
RAYDAN GASPAR <raydangaspar@gmail.com>
2020-05-30 14:43:09 -0300

    Atualizando dados no servidor

--- jogoteca.py
+++ jogoteca.py
@@ -18,6 +18,7 @@ db = MySQL(app)
 jogo_dao = JogoDao(db)
 usuario_dao = UsuarioDao(db)
 
+
 @app.route('/')
 def index():
     lista = jogo_dao.listar()
@@ -41,16 +42,24 @@ def criar():
     jogo_dao.salvar(jogo)
     return redirect(url_for('index'))
 
+
 @app.route('/editar/<int:id>')
 def editar(id):
-    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('editar')))
     jogo = jogo_dao.busca_por_id(id)
     return render_template('editar.html', titulo='Editando jogo', jogo=jogo)
 
-@app.route('/atualizar', methods=['POST', ])
+
+@app.route('/atualizar', methods=['POST',])
 def atualizar():
-    pass
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console, id=request.form['id'])
+    jogo_dao.salvar(jogo)
+    return redirect(url_for('index'))
+
 
 @app.route('/login')
 def login():
